f8983be325c5ef56c1e18a16b1a11fe88d95a0ff
Dhanush-734 <dhanushs2366@gmail.com>
2026-04-26 10:06:51 +0530

    Delete smart directory

--- smart/backend/test_db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine("mysql+mysqlconnector://root:devi7977%40dhanush@localhost:3306/smart_ecommerce")
-
-conn = engine.connect()
-print("✅ Connected successfully!")
-conn.close()
